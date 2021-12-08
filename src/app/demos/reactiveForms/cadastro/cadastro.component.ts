4f5a377f5ba94cc4b0f7a92f1369356ee2b7f148
emanuellyogg <manuogg90@gmail.com>
2021-12-08 16:16:08 -0300

    feat(cadastro): cria validação básica de formulário no campo nome e email

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
+import { Usuario } from './models/usuario';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,20 +9,27 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup;
+  usuario: Usuario;
+  formResult: string = '';
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.cadastroForm = this.fb.group({
-      nome: [''],
+      nome: ['', Validators.required],
       cpf: [''],
-      email: [''],
+      email: ['', [Validators.required, Validators.email]],
       senha: [''],
       senhaConfirmacao: ['']
     });
   }
 
   adicionarUsuario() {
-    let x = this.cadastroForm.value;
+    if (this.cadastroForm.dirty && this.cadastroForm.valid) {
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+      this.formResult = JSON.stringify(this.cadastroForm.value);
+    } else {
+      this.formResult = "Não submeteu!!!"
+    }
   }
 }
