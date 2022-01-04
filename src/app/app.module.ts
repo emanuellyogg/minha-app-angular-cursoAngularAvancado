3aa8ffb2dd97fa84bc5b4721b500614940a5411a
emanuellyogg <manuogg90@gmail.com>
2022-01-04 15:58:07 -0300

    feat(navegacao.module): cria o submodulo de navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NavegacaoModule } from './navegacao/navegacao.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -5,9 +6,6 @@ import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { rootRouterConfig } from './app.routes';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
@@ -19,15 +17,13 @@ import { CustomFormsModule } from 'ng2-validation'
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     SobreComponent,
     CadastroComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    NavegacaoModule,
     ReactiveFormsModule,
     NgBrazil,
     TextMaskModule,
