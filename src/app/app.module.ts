117a02f30175600e0297ae217bb803348cf7b098
emanuellyogg <manuogg90@gmail.com>
2022-01-05 09:48:13 -0300

    feat(app.router): aplica o módulo de roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NavegacaoModule } from './navegacao/navegacao.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
-import { rootRouterConfig } from './app.routes';
+// import { rootRouterConfig, AppRoutingModule } from './app.routes';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 
 import { NgBrazil } from 'ng-brazil';
 import { TextMaskModule } from 'angular2-text-mask';
 import { CustomFormsModule } from 'ng2-validation'
+
+import { AppRoutingModule } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { CustomFormsModule } from 'ng2-validation'
     NgBrazil,
     TextMaskModule,
     CustomFormsModule,
-    [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
+    AppRoutingModule
+    // [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
   ],
   providers: [
     { provide: APP_BASE_HREF, useValue: '/' }
