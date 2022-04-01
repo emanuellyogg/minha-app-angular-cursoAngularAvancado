630b7bb255e1302514f43ae076d54f5a84a9f827
emanuellyogg <manuogg90@gmail.com>
2022-04-01 08:50:50 -0300

    feat: atualizado as imagens no assets; Gerenciando Eventos de Mudança de Estado

--- src/app/demos/todo-list/todo.service.ts
+++ src/app/demos/todo-list/todo.service.ts
@@ -1,9 +1,9 @@
-import { Store } from './todo.store';
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Task } from './task';
 import { tap } from 'rxjs/operators';
+import { Task } from './task';
+import { Store } from './todo.store';
 
 @Injectable()
 export class TasksService {
@@ -19,4 +19,23 @@ export class TasksService {
   //   return this.http
   //     .get<Task[]>('http://localhost:3000/todolist');
   // }
+
+  toggle(event: any) {
+    this.http
+      .put(`http://localhost:3000/todolist/${event.task.id}`, event.task)
+      .subscribe(() => {
+
+        const value = this.store.value.todolist;
+
+        const todolist = value.map((task: Task) => {
+          if (event.task.id === task.id) {
+            return { ...task, ...event.task }
+          } else {
+            return task;
+          }
+        });
+
+        this.store.set('todolist', todolist);
+      })
+  }
 }
