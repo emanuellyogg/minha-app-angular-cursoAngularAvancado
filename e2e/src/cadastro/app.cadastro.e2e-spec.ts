aceefb237e60a6aad7041d5de01f1ce15597879f
emanuellyogg <manuogg90@gmail.com>
2022-04-27 09:56:45 -0300

    feat: testes e2e parte 3

--- e2e/src/cadastro/app.cadastro.e2e-spec.ts
+++ e2e/src/cadastro/app.cadastro.e2e-spec.ts
@@ -8,6 +8,37 @@ describe('Testes do formulário de cadastro', () => {
     page = new AppCadastroPage();
   });
 
+  it('Deve navegar até formulário de cadastro', () => {
+    page.iniciarNavegação();
+    expect(page.obterTituloCadastro()).toEqual('Demo Cadastro');
+  });
+
+  it('deve preencher o formulário de cadastro com sucesso', () => {
+    page.campoNome.sendKeys('Emanuelly Ogg');
+    page.campoCPF.sendKeys('04502646962');
+    page.campoEmail.sendKeys('teste@teste.com');
+    page.campoSenha.sendKeys('Teste@123');
+    page.campoSenhaConfirmacao.sendKeys('Teste@123');
+
+    page.botaoRegistrar.click();
+    page.esperar(1000);
+
+    expect(page.obterResultadoCadastro()).toContain('"nome":"Emanuelly Ogg"');
+  });
+
+  it('deve validar senhas diferentes', () => {
+    page.campoNome.sendKeys('Emanuelly Ogg');
+    page.campoCPF.sendKeys('04502646962');
+    page.campoEmail.sendKeys('teste@teste.com');
+    page.campoSenha.sendKeys('Teste@2123');
+    page.campoSenhaConfirmacao.sendKeys('Teste@123');
+
+    page.campoSenha.click();
+    page.esperar(1000);
+
+    expect(page.obterErroSenha()).toContain('As senhas não conferem');
+  });
+
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
